67b2bf634814986d41e0742d692ddb78ef9162cc
Alexey Sodnomov <sodnomoff@gmail.com>
2020-10-05 03:00:31 +0800

    init

---
+++ android/app/src/main/kotlin/com/sodnmoff/clear_structure/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sodnmoff.clear_structure
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,8 @@
+import 'package:clear_structure/core/error/failures.dart';
+import 'package:clear_structure/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class NumberTriviaRepository {
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+}
