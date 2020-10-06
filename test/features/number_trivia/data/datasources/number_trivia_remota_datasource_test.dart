4c3c02096899f9b6224aa51e888d342389d70ac1
Alexey Sodnomov <sodnomoff@gmail.com>
2020-10-07 02:29:01 +0800

    remote datasource

--- test/features/number_trivia/data/datasources/number_trivia_remota_datasource_test.dart
+++ test/features/number_trivia/data/datasources/number_trivia_remota_datasource_test.dart
@@ -1,6 +1,13 @@
+import 'dart:convert';
+
+import 'package:clear_structure/core/error/exceptions.dart';
+import 'package:clear_structure/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart';
+import 'package:clear_structure/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
+
+import '../../../../fixtures/fixture_reader.dart';
 
 class MockHttpClient extends Mock implements http.Client {}
 
@@ -11,5 +18,120 @@ void main() {
   setUp(() {
     mockHttpClient = MockHttpClient();
     dataSource = NumberTriviaRemoteDataSourceImpl(client: mockHttpClient);
+  });
+
+  void setUpMockHttpClientSuccess200() {
+    when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+            (_) async => http.Response(fixture('trivia.json'), 200));
+  }
+
+  void setUpMockHttpClientFailure404() {
+    when(mockHttpClient.get(any, headers: anyNamed('headers')))
+        .thenAnswer((_) async => http.Response('All wrong', 404));
+  }
+
+  group('getConcreteNumberTrivia', () {
+    final tNumber = 1;
+    final tNumberTriviaModel = NumberTriviaModel.fromJson(
+      json.decode(
+        fixture('trivia.json'),
+      ),
+    );
+    test(
+        'should perform a GET request on URL '
+        'with number being the endpoint and with application/json header', () {
+      // arrange
+      setUpMockHttpClientSuccess200();
+      // act
+      dataSource.getConcreteNumberTrivia(tNumber);
+      // assert
+      verify(mockHttpClient.get(
+        'http://numbersapi.com/$tNumber',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      ));
+    });
+
+    test(
+      'should return NumberTrivia when the response code is 200 (success)',
+      () async {
+        // arrange
+        setUpMockHttpClientSuccess200();
+        // act
+        final result = await dataSource.getConcreteNumberTrivia(tNumber);
+        // assert
+        expect(result, equals(tNumberTriviaModel));
+      },
+    );
+
+    test(
+      'should throw a ServerException when the response code is 404 or other',
+      () async {
+        // arrange
+        setUpMockHttpClientFailure404();
+        // act
+        final call = dataSource.getConcreteNumberTrivia;
+        // assert
+        expect(
+          () => call(tNumber),
+          throwsA(
+            predicate((e) => e is ServerException),
+          ),
+        );
+      },
+    );
+  });
+
+  group('getRandomNumberTrivia', () {
+    final tNumberTriviaModel = NumberTriviaModel.fromJson(
+      json.decode(
+        fixture('trivia.json'),
+      ),
+    );
+    test(
+        'should perform a GET request on URL '
+            'with number being the endpoint and with application/json header', () {
+      // arrange
+      setUpMockHttpClientSuccess200();
+      // act
+      dataSource.getRandomNumberTrivia();
+      // assert
+      verify(mockHttpClient.get(
+        'http://numbersapi.com/random',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      ));
+    });
+
+    test(
+      'should return NumberTrivia when the response code is 200 (success)',
+          () async {
+        // arrange
+        setUpMockHttpClientSuccess200();
+        // act
+        final result = await dataSource.getRandomNumberTrivia();
+        // assert
+        expect(result, equals(tNumberTriviaModel));
+      },
+    );
+
+    test(
+      'should throw a ServerException when the response code is 404 or other',
+          () async {
+        // arrange
+        setUpMockHttpClientFailure404();
+        // act
+        final call = dataSource.getRandomNumberTrivia;
+        // assert
+        expect(
+              () => call(),
+          throwsA(
+            predicate((e) => e is ServerException),
+          ),
+        );
+      },
+    );
   });
 }
