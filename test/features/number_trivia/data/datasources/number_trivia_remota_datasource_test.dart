7a1dd083c8755d2ae88eb52cbe86c4670f357a6e
Alexey Sodnomov <sodnomoff@gmail.com>
2020-10-06 22:01:56 +0800

    remote datasource

---
+++ test/features/number_trivia/data/datasources/number_trivia_remota_datasource_test.dart
@@ -0,0 +1,15 @@
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+
+class MockHttpClient extends Mock implements http.Client {}
+
+void main() {
+  NumberTriviaRemoteDataSourceImpl dataSource;
+  MockHttpClient mockHttpClient;
+
+  setUp(() {
+    mockHttpClient = MockHttpClient();
+    dataSource = NumberTriviaRemoteDataSourceImpl(client: mockHttpClient);
+  });
+}
